b673deefcc21f8ac2076904615dedab37edb535a
Scott F <sfrieds3@gmail.com>
2024-08-06 22:05:57 -0500

    separate config

---
+++ lua/easynote/config.lua
@@ -0,0 +1,24 @@
+local Config = {}
+
+---@alias DefaultPickerOptions "fzf"
+---@alias NotesScope "local" | "global"
+
+---@class NotesConfiguration
+---@field notes_dir string
+---@field notes_filenames table[string]
+---@field default_global_notes_file string|nil
+---@field use_default_file boolean
+---@field default_picker DefaultPickerOptions
+---@field default_notes_scope NotesScope
+---@field dir_markers table[string] list of strings which will be used to mark the project root
+Config.config = {
+  notes_dir = "~/wiki",
+  notes_filenames = { "notes.md" },
+  default_file = nil,
+  use_default_file = true,
+  default_picker = "fzf",
+  default_notes_scope = "global",
+  dir_markers = { ".git" },
+}
+
+return Config
